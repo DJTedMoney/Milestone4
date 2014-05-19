e3522ad6d64c555a72cdc04602ea78ab211a3520
jheckard <jason.heckard@yahoo.com>
2014-05-18 21:49:08 -0700

    begin impelementing game manager on server side

--- ServerDatabase/ServerDatabase/GameMechanicsManager.cs
+++ ServerDatabase/ServerDatabase/GameMechanicsManager.cs
@@ -8,5 +8,37 @@ namespace ServerDatabase
 {
     class GameMechanicsManager
     {
+        Pellet[] gamePellets = new Pellet[4];
+
+        Player[] gamePlayers = new Player[4];
+
+        Random roller;
+
+        // send is used to determine if the game manager is sending a message to the TCPServer or not
+        bool send;
+
+        // command is the instruction that 
+        string command;
+
+        GameMechanicsManager()
+        {
+            roller = new Random();
+
+            // counting by p
+            for (int p = 0; p < 4; p++)
+            {
+                int startX = roller.Next(-450, 450);
+                int startY = roller.Next(-450, 450);
+
+                gamePellets[p].setPosition(startX, startY);
+            }
+
+            // counting players by l
+            for (int l = 0; l < 4; l++)
+            {
+
+            }
+        }
+
     }
 }
