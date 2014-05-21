ddfc9c75592637d3ef2b8db9c21c4bcca4f5eff4
Nathan Stengel <clockworkElm@hotmail.com>
2014-05-21 04:06:18 -0700

    isActive sets to true now

--- Whale/Assets/Scripts/GameManager.cs
+++ Whale/Assets/Scripts/GameManager.cs
@@ -121,6 +121,7 @@ public class GameManager : MonoBehaviour
 				guiBox.grafxText.text = "Connected\nWelcome back " + guiBox.userName;
 				clientNumber = int.Parse(tempCommand.Substring(0,tempCommand.IndexOf(delim)));
 				tempCommand = tempCommand.Substring(tempCommand.IndexOf(delim)+1);
+				isActive[clientNumber] = true;
 			}
 			//Server sent Move commands to client
 			if(comType.Equals("2") && move == true)
